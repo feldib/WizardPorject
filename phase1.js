1cc1f7e3cae0163d6f26252dbe4fe24fe86b3a8a
feldib <beni.feldmayer@gmail.com>
2023-05-10 15:20:15 +0200

    adding branch phase1

--- phase1.js
+++ phase1.js
@@ -1,4 +1,5 @@
 const birthDate = document.querySelector("#birthDate")
+const form = document.querySelector("form")
 
 function fillInDashes(event){
     let value = event.target.value
@@ -6,5 +7,13 @@ function fillInDashes(event){
         event.target.value += "/"
     }
 }
+
+form.addEventListener("submit", function(event){
+    event.preventDefault()
+    const formElements = Array.from(form.elements)
+    storage.setItem("name", formElements[0].value)
+    storage.setItem("email", formElements[1].value)
+    storage.setItem("birthdate", formElements[2].value)
+})
 
 birthDate.addEventListener("keyup", fillInDashes)
