77e41080fc0dcbf59fa4ce1ba0186b0a01432c4b
Miles Davis <foldes@mail.com>
2023-05-10 13:25:09 +0200

    common.js changed, phase 3 uploaded.

---
+++ phase_3.js
@@ -0,0 +1,31 @@
+/* functions for phase 3 */
+//import storage from "./common"
+
+//get imgURL from  the input
+const imgInputField = document.querySelector('#image-url');
+function getImgURL(){
+    const inputValue = imgInputField.value;
+    storage.setItem('image', inputValue);
+};
+
+//get hobbies array
+function trueHobbies(){
+    const hobbiesField = document.querySelectorAll('input[name="hobby"]:checked');
+    const checkedHobbiesArr = [];
+    for (let i = 0; i < hobbiesField.length; i++){
+        checkedHobbiesArr.push(hobbiesField[i].value);
+    };
+
+    hobbieString = ''
+    for (let i = 0; i < checkedHobbiesArr.length; i++){
+        hobbieString += ` ${checkedHobbiesArr[i]},`;
+    };
+
+    return hobbieString;
+};
+
+function getHobbies(){
+    const hobbies = trueHobbies();
+    storage.setItem('hobbies', hobbies);
+};
+
